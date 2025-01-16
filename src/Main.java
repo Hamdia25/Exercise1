1338d304400ffe0da4e8aafb692a1f2284ca5a1b
Hamdi I Aden 11th WHS <109240@SWCSD.US>
2025-01-16 14:40:49 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    // Hamdi Aden CSCI 1660 1/16/2025
+        String city = "Columbus";
+        int zipCode = 43215;
+        int[] highTemp = {32, 25, 27, 40, 45};
+        double avg = 33.8;
+        System.out.println("City: " + city + " " + " " + "Zip Code: " + zipCode + " " + "Average High Temperature: " + avg);
+    }
+
+}
