8dd1d47112c04d3cb30c349672ac0dff0b2de6a8
Hamdia25 <hamdiaden033@gmail.com>
2025-01-19 19:50:30 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
     // Hamdi Aden CSCI 1660 1/16/2025
         String city = "Columbus";
         int zipCode = 43215;
         int[] highTemp = {32, 25, 27, 40, 45};
-        double avg = 33.8;
+        int sum = 0;
+        int i;
+        for (i = 0; i < highTemp.length; i++) {
+            sum += highTemp[i];
+        }
+        double avg = (double) sum / highTemp.length;
         System.out.println("City: " + city + " " + " " + "Zip Code: " + zipCode + " " + "Average High Temperature: " + avg);
-    }
 
-}
+        }
+    }
